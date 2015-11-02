ce08b9a5680f345f27ca00a4612abdc2eaa40e57
Paul Everitt <paul@agendaless.com>
2015-11-02 17:20:33 -0500

    Yikes, a typo.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['pyramid >= 1.2', 'cryptography', 'PyJWT']
 
 setup(name='pyramid_jwtauth',
-      version='0.1.3+agendalessLet's,
+      version='0.1.3+agendalessLet'Y           ,
       description='pyramid_jwtauth',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
